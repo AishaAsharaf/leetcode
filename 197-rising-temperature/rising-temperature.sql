9da38759ba80ced430b8826c74e1976b9be46a5f
Aisha M Asharaf <97108251+AishaAsharaf@users.noreply.github.com>
2026-08-01 13:45:17 +0200

    Time: 1192 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 select w2.id
  from weather w1 join weather w2 
- on datediff(day,w1.recordDate,w2.recordDate)=1 
+ on  w2.recordDate = DATEADD(day, 1, w1.recordDate)
   and w1.temperature<w2.temperature;
