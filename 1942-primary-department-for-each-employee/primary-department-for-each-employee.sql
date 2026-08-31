4cfee171447fc32af2c8032afc3d4769ed6ef854
Aisha M Asharaf <97108251+AishaAsharaf@users.noreply.github.com>
2026-08-31 13:09:24 +0200

    Time: 669 ms (31.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT e.employee_id,
+       e.department_id
+FROM Employee e
+WHERE e.primary_flag = 'Y'
+
+   OR e.employee_id IN (
+       SELECT employee_id
+       FROM Employee
+       GROUP BY employee_id
+       HAVING COUNT(*) = 1
+   )
+ORDER BY e.employee_id;
